91db31c1a6f0ccdac633de2356b45a73231c8f4f
progiv <gmvano@gmail.com>
2018-05-21 02:30:18 +0300

    Moves function to external files

---
+++ richardson_lucy.py
@@ -0,0 +1,105 @@
+import numpy as np
+from scipy.signal import fftconvolve, convolve2d
+from scipy.stats.stats import pearsonr
+
+def row_correlation(image):
+    return pearsonr(image[1:,:].ravel(), image[:-1,:].ravel())[0]
+    
+def col_correlation(image):
+    return pearsonr(image[:,1:].ravel(), image[:,:-1].ravel())[0]
+
+
+def corelucy(Y, psf, dampar22, wI, readout, eps=1e-9, convolvemethod=convolve2d):
+    """
+    CORELUCY Accelerated Damped Lucy-Richarson Operator.
+    Calculates function that when used with the scaled projected array
+    produces the next iteration array that maximizes the likelihood that
+    the entire suite satisfies the Poisson statistics.
+    """
+    reBlurred = np.real(convolvemethod(Y, psf, 'same'))
+
+    # 2. An Estimate for the next step
+    reBlurred += readout;
+    reBlurred[reBlurred <= 0] = eps;
+    #reBlurred[reBlurred < eps] = eps;
+    AnEstim = wI / reBlurred + eps;
+
+    # 3. Damping if needed
+    if dampar22 == 0: # No Damping
+        ImRatio = AnEstim
+    else: # Damping of the image relative to dampar22 = (N*sigma)^2
+        gm = 10;
+        g = (wI * np.log(AnEstim)+ reBlurred - wI) / dampar22;
+        g = np.minimum(g,1);
+        G = (g**(gm-1))*(gm-(gm-1)*g);
+        ImRatio = 1 + G * (AnEstim - 1);
+    return ImRatio
+
+def richardson_lucy_matlab(image, psf, iterations=50, dampar=0, weight=None, readout=0, 
+                           eps=1e-16, clip=True, useFFT=False):
+    """ Richardson-Lucy deconvolution.
+
+    Parameters
+    ----------
+    image : ndarray
+       Input degraded image (can be N dimensional).
+    psf : ndarray
+       The point spread function.
+    iterations : int
+       Number of iterations. This parameter plays the role of
+       regularisation.
+    """
+    psf_mirror = psf[::-1,::-1]
+    
+    assert(not np.all(psf == 0))
+
+    if isinstance(image, list) and len(image) == 4:
+        image, prev_image, prev_prev_image, internal = image
+    else:
+        prev_image = image.copy()#np.zeros(image.shape)*.5#image
+        prev_prev_image = 0
+        internal = np.zeros((image.size,2))
+    internal[:,1] = 0
+
+    if weight is None:
+        weight = np.ones(image.shape)
+
+    wI = np.maximum(weight * (readout + image), 0)
+    if useFFT:
+        convolvemethod = fftconvolve
+    else:
+        convolvemethod = convolve2d
+    scale = convolve2d(weight, psf, 'same') + np.sqrt(eps)
+    del weight
+
+    dampar22 = np.square(dampar)/2
+
+    # 3 L_R Iterations
+    lambd = 2 * np.any(internal != 0)
+    correlation_X = [col_correlation(image)]
+    correlation_Y = [row_correlation(image)]
+    for k in range(lambd, lambd + iterations):
+        # 3.a Make an image predictions for the next iteration
+        if k > 2:
+            lambd = (internal[:,0].T.dot(internal[:,1])) / (internal[:,1].T.dot(internal[:,1]) + eps) # (scalar division)
+            lambd = np.maximum(np.minimum(lambd, 1), 0) # stability enforcement saturation
+        Y = np.maximum(prev_image + lambd*(prev_image - prev_prev_image), 0) # plus positivity constraint
+
+        # 3.b Make core for the LR estimation
+        cc = corelucy(Y, psf, dampar22, wI, readout, eps, convolvemethod)
+        
+        # 3.c Determine next iteration image and apply poitivity constraint
+        prev_prev_image = prev_image.copy()
+        prev_image = np.maximum(Y * np.real(convolvemethod(cc, psf_mirror, 'same')) / scale, 0)#np.conj(psf)
+        if clip:
+            prev_image[prev_image > 1] = 1
+            prev_image[prev_image < eps] = eps
+        correlation_X.append(col_correlation(prev_image))
+        correlation_Y.append(row_correlation(prev_image))
+        del cc
+        internal[:,1] = internal[:,0]
+        internal[:,0] = (prev_image-Y).ravel()
+    del wI, scale, Y
+    return {'image': prev_image, 
+            'correlationX': np.array(correlation_X)/(image.size-1), 
+            'correlationY': np.array(correlation_Y)/(image.size-1)}
---
+++ util.py
@@ -0,0 +1,217 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.signal import convolve2d #fftconvolve
+from numpy.fft import fftn, ifftn
+from scipy.ndimage import filters
+from itertools import chain
+from skimage.draw import line_aa
+from math import sin, cos, pi, factorial
+import math
+
+def edgetaper(I, psf):
+    """
+    J = EDGETAPER(I,PSF) blurs the edges of image I using the point-spread
+    function, PSF. The output image J is the weighted sum of the original
+    image I and its blurred version. The weighting array, determined by the
+    autocorrelation function of PSF, makes J equal to I in its central
+    region, and equal to the blurred version of I near the edges.
+    
+    The EDGETAPER function reduces the ringing effect in image deblurring
+    methods that use the discrete Fourier transform, such as DECONWNR,
+    DECONVREG, and DECONVLUCY.
+    
+    Note that the size of the PSF cannot exceed half of the image size in any
+    dimension.
+    """
+    # [I, PSF, sizeI, classI, sizePSF, numNSdim] = parse_inputs(varargin{:});
+    if np.all(psf >= 0):
+        psf /= psf.sum()
+    # PSF size cannot be larger than sizeI/2 because windowing is performed
+    # with PSF autocorrelation function
+    if np.any(np.array(I.shape) <= np.array(psf.shape) * 2):
+        raise ValueError(psf)
+
+    numDim = len(psf.shape)
+    beta = [0] * numDim
+    for n in range(numDim):
+        PSFproj = np.sum(psf, axis=n)
+        z = np.real(ifftn(np.abs(fftn(PSFproj, (I.shape[n] - 1,)))**2))
+        z = z/np.max(z)
+        beta[n] = np.append(z, z[0])
+    
+    if numDim == 1:
+        alpha = 1 - beta[0]
+    else: # n == 2:
+        alpha = np.matmul((1-beta[0]).reshape((I.shape[0], 1)), (1 - beta[1]).reshape(1, I.shape[1]))
+    # n > 2 - not implemented
+    
+    blurred = convolve2d(I, psf, 'same', 'wrap')#, 'symm')
+    J = alpha * I + (1-alpha)*blurred
+
+    return np.clip(J, np.min(I), np.max(I))
+
+def show_results(original, noisy, result, titles=['Original Data', 'Blurred data', 'Restoration using\nRichardson-Lucy']):
+    # Show results
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(12, 10))
+    plt.gray()
+
+    for a in (ax[0], ax[1], ax[2]):
+           a.axis('off')
+
+    ax[0].imshow(original)
+    ax[0].set_title(titles[0])
+
+    ax[1].imshow(noisy)
+    ax[1].set_title(titles[1])
+
+    ax[2].imshow(result)#, vmin=noisy.min(), vmax=noisy.max())
+    ax[2].set_title(titles[2])
+
+
+    fig.subplots_adjust(wspace=0.02, hspace=0.2,
+                        top=0.9, bottom=0.05, left=0, right=1)
+    plt.show()
+
+def gkern2(kernlen=21, nsig=3):
+    """Returns a 2D Gaussian kernel array."""
+
+    # create nxn zeros
+    inp = np.zeros((kernlen, kernlen))
+    # set element at the middle to one, a dirac delta
+    inp[kernlen//2, kernlen//2] = 1
+    # gaussian-smooth the dirac, resulting in a gaussian filter mask
+    return filters.gaussian_filter(inp, nsig)
+
+def plot_corr(n, ydata, legend=['row correlation', 'column correlation'], title='Normalized correlation'):
+    plt.plot(*list(chain(*[(range(n), y) for y in ydata])))
+    plt.grid()
+    plt.title(title)
+    plt.xlabel('n iterations')
+    plt.legend(legend)
+    plt.show()
+
+def motion_blur_psf(length, angle):
+    cc, rr, val = line_aa(0, 0, int(length*cos(angle)), int(length*sin(angle)))
+    psf = np.zeros((max(rr)+1, max(cc)+1))
+    psf[rr, cc] = val
+    return psf/np.sum(psf)
+
+def motion_blur_psf_my(length=10, angle=pi/4, n_points=1000, **kwargs):
+    """Incorrectly works with angles k*pi/2, where k is integer."""
+    x_start, y_start = 0, 0
+    if 'x' in kwargs and 'y' in kwargs:
+        x_end, y_end = kwargs['x'], kwargs['y']
+    else:
+        x_end, y_end = length*cos(angle), length*sin(angle)
+    if x_end < 0:
+        x_start -= x_end
+        x_end = 0
+    if y_end < 0:
+        y_start -= y_end
+        y_end = 0
+    psf = np.zeros((int(max(y_start, y_end))+2, int(max(x_start, x_end))+2))
+    
+    triangle_fun = lambda x: np.maximum(0, (1 - np.abs(x)))
+    triangle_fun_prod = lambda x, y: np.multiply(triangle_fun(x), triangle_fun(y))
+    
+    X = np.linspace(x_start, x_end, n_points)
+    Y = np.linspace(y_start, y_end, n_points)
+    x1 = np.floor(X).astype(np.int)
+    x2 = x1+1
+    y1 = np.floor(Y).astype(np.int)
+    y2 = y1+1
+    print(x_start, y_start, x_end,y_end)
+    psf[y1, x1] += triangle_fun_prod(X - x1, Y - y1)
+    psf[y2, x1] += triangle_fun_prod(X - x1, Y - y2)
+    psf[y1, x2] += triangle_fun_prod(X - x2, Y - y1)
+    psf[y2, x2] += triangle_fun_prod(X - x2, Y - y2)
+    return psf/np.sum(psf)
+
+def find_noise(image):
+    N, M = image.shape
+    F = fftn(image)
+    part = .02
+    area = F[int(N/2-N*part):int(N/2+N*part), int(M/2-M*part):int(M/2+M*part)]
+    np.std
+    np.var
+    msv = np.sqrt(np.mean(np.abs(area) ** 2) / (N*M))
+    I_res = ifftn(F-msv)
+    NSR = msv**2 / np.var(I_res)
+    return (msv, NSR)
+
+def binomial(i, n):
+    """Binomial coefficient"""
+    return factorial(n) / float(
+        factorial(i) * factorial(n - i))
+
+
+def bernstein(t, i, n):
+    """Bernstein polynom"""
+    return binomial(i, n) * (t ** i) * ((1 - t) ** (n - i))
+
+
+def bezier(t, points):
+    """Calculate coordinate of a point in the bezier curve"""
+    n = len(points) - 1
+    x = y = 0
+    for i, pos in enumerate(points):
+        bern = bernstein(t, i, n)
+        x += pos[0] * bern
+        y += pos[1] * bern
+    return int(x), int(y)
+
+
+def bezier_curve_range(n, points):
+    """Range of points in a curve bezier"""
+    for i in range(n):
+        t = i / float(n - 1)
+        yield bezier(t, points)
+
+def bezier_psf(points, n=100):
+    print(points)
+    curve = bezier_curve_range(n, points)
+    r, c = zip(*[p for p in curve])
+    psf = np.zeros((int(np.ceil(np.max(r)+1)), int(np.ceil(np.max(c)+1))))
+    for rr, cc in zip(r,c):
+        psf[rr, cc] += 1
+#     psf[r,c] = 1
+    psf /= psf.sum()
+    return psf
+
+def bezier_psf2(points, n=100):
+    x = [0] + points[::2]
+    y = [0] + points[1::2]
+    xy = list(zip(x, y))
+    return bezier_psf(xy, n=n)
+
+def curved_psf():
+    eps = 1e-16
+    p0 = np.random.randint(6, 25)
+    p1 = np.random.randint(6, 21)
+
+    x = np.random.randint(0, p0, 4)#sort(randi([0 p0], 1, 4));
+    y = np.random.randint(0, p1, 4)
+
+    pt1 = [[x[0]], [y[0]]]
+    pt2 = [[x[1]], [y[1]]]
+    pt3 = [[x[2]], [y[2]]]
+    pt4 = [[x[3]], [y[3]]]
+
+    t = np.linspace(0, 1, p0)
+    pts = np.kron((1-t) ** 3, pt1) + np.kron(3*(1-t)**2 * t, pt2) + np.kron(3 * (1-t) * t**2, pt3) + np.kron(t**3, pt4)
+    x1 = np.arange(1, p0+1)
+    y1 = np.round(pts[1, :]).astype(np.int64)
+    y1 = y1 - min(y1) + 1
+    #plot(x1,y1)
+    pp = np.max(y1)
+    PSF = np.zeros((p0, len(x1)))
+
+    print(len(x1), PSF.shape)
+
+    for ind_x in range(len(x1)):
+        yy = max(len(x1) - y1[ind_x], 1)
+        PSF[yy, ind_x] = np.random.rand()
+
+    PSF = PSF/(sum(PSF.ravel()) + eps*p0*p0)
+    return PSF
